6f6197079e9d89fdeebd992cddad1bfab7163f35
Simone Carella <simone.carella@link-me.it>
2020-01-09 13:55:14 +0100

    Add support for uniforms

--- shaderino.js
+++ shaderino.js
@@ -1,9 +1,9 @@
-const vs = `
+const VERTEX_SHADER = `
   attribute vec4 a_position;
 
   void main() { 
     gl_Position = a_position;
-  } 
+  }
 `
 
 function createShader(gl, type, source) {
@@ -37,42 +37,95 @@ function createProgram(gl, vertexShader, fragmentShader) {
     gl.deleteProgram(program)
 }
 
-function shaderino(canvasEl, fs) {
+function tex2D(gl, image, id = 0) {
+    let texture = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+    return gl.texImage2D(gl.TEXTURE_2D, id, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
+}
+
+function setupProgram(gl, fs) {
+    let vertexShader = createShader(gl, gl.VERTEX_SHADER, VERTEX_SHADER)
+    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, `
+        precision highp float;
+        uniform vec2 u_resolution;
+        uniform float u_time;
+
+        ${fs}
+    `)
+
+    return createProgram(gl, vertexShader, fragmentShader)
+}
+
+function setupUniforms(gl, program, uniforms) {
+    return uniforms.reduce((a, b) => {
+        return {
+            ...a,
+            [b.name]: uniform(gl, program, b.type, b.name, b.update),
+        }
+    }, {})
+}
+
+function uniform(gl, program, type, name, updater) {
+    let loc = gl.getUniformLocation(program, name)
+    let __value
+    return {
+        update: updater,
+        get() {
+            return __value
+        },
+        set(value) {
+            __value = value
+            switch (type) {
+                case "f":
+                    return gl.uniform1f(loc, value)
+                case "vec2":
+                    return gl.uniform2f(loc, ...value)
+                case "vec3":
+                    return gl.uniform2f(loc, ...value)
+                case "tex2D":
+                    console.log("texture2d uniform type not supported yet!")
+                    break
+                    //return tex2D(gl, value, 0)
+                default:
+                    break
+            }
+        }
+    }
+}
+
+
+function shaderino(canvasEl, fs, uniforms = []) {
     let gl = canvas.getContext("webgl")
+    let time = 0
+    let running = false
+    let rafId
 
     if (!gl) {
         console.error("No webgl support")
         return
     }
 
-    let vertexShader = createShader(gl, gl.VERTEX_SHADER, vs)
-    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, `
-        precision highp float; 
-        uniform vec2 u_resolution; 
-        uniform float u_time;
-
-        ${fs}
-    `)
-
-    let program = createProgram(gl, vertexShader, fragmentShader)
-
+    let program = setupProgram(gl, fs)
     let positionAttributeLocation = gl.getAttribLocation(program, "a_position")
-    let resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
-    let timeUniformLocation = gl.getUniformLocation(program, "u_time")
+    let resolutionUniform = uniform(gl, program, "vec2", "u_resolution")
+    let timeUniform = uniform(gl, program, "f", "u_time")
+    let userUniforms = setupUniforms(gl, program, uniforms)
 
     let positionBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
     // one big triangle
-    let positions = [
-        -2, 2,
-        3, 1,
-        -1.0, -3,
-    ]
-
+    let positions = [-2, 2, 3, 1, -1, -3]
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
     function draw() {
+        // Set size
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
         // Clear the canvas
@@ -87,51 +140,41 @@ function shaderino(canvasEl, fs) {
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
         // Set the resolution uniform
-        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
-
+        resolutionUniform.set([gl.canvas.width, gl.canvas.height])
+        
         // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        let size = 2          // 2 components per iteration
-        let type = gl.FLOAT   // the data is 32bit floats
-        let normalize = false // don't normalize the data
-        let stride = 0        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        let offset = 0        // start at the beginning of the buffer
-
         gl.vertexAttribPointer(
-            positionAttributeLocation, size, type, normalize, stride, offset)
+            positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
 
-        let primitiveType = gl.TRIANGLES
-        let count = 3
-        offset = 0
-
-        gl.drawArrays(primitiveType, offset, count)
+        gl.drawArrays(gl.TRIANGLES, 0, 3)
     }
-
-    let time = 0
-    let rafId
-    let looping = false
 
     function update() {
-        if (!looping) {
+        if (!running) {
             return
         }
-        
-        time += 0.01
 
         // Set time uniform
-        gl.uniform1f(timeUniformLocation, time)
-        draw()
-        
-        rafId = requestAnimationFrame(function () {
-            update()
+        timeUniform.set(time)
+        time += 0.01
+
+        // Update user uniforms
+        Object.keys(userUniforms).forEach(name => {
+            userUniforms[name].set(userUniforms[name].update())
         })
+
+        draw()
+
+        rafId = requestAnimationFrame(() => update())
     }
+
     return {
         stop() {
-            looping = false
+            running = false
             cancelAnimationFrame(rafId)
         },
-        draw() {
-            looping = true
+        start() {
+            running = true
             draw()
             update()
         }
